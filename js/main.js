63bdf38c0ae98a1df5a12e812b1072035363a996
Nelo <nemuriael@gmail.com>
2022-10-17 21:46:22 +0200

    Search bar style and logic
    Developed the logic behind searching a message between chanels and styled it (without message bubbles y

--- js/main.js
+++ js/main.js
@@ -1,81 +1,118 @@
-const chatForm = document.getElementById('chat-form');
-const messageContainer = document.querySelector('.chat-messages');
+const chatForm = document.querySelector("#chat-form");
+const messageContainer = document.querySelector(".chat-messages");
+const searchBar = document.querySelector(".search-bar");
 
-chatForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const msgElement = e.target.elements.msg;
-    const today = new Date();
-    const msg = {
-        username: 'Usuario',
-        time: `${String(today.getHours()).padStart(2, "0")}:${String(today.getMinutes()).padStart(2, "0")}`,
-        text: msgElement.value
-    };
+chatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const msgElement = e.target.elements.msg;
+  const today = new Date();
+  const msg = {
+    username: "Usuario",
+    time: `${String(today.getHours()).padStart(2, "0")}:${String(
+      today.getMinutes()
+    ).padStart(2, "0")}`,
+    text: msgElement.value,
+  };
 
-    channels[currentChannel].messages.push(msg)
-    outputMessages(msg);
-    msgElement.value = '';
-    msgElement.focus();
+  channels[currentChannel].messages.push(msg);
+  outputMessages(msg);
+  msgElement.value = "";
+  msgElement.focus();
 });
 
+searchBar.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchElement = e.target.elements.search;
+  const searchTerm = searchElement.value;
+  const searchContainer = document.querySelector("#search-container");
+  searchContainer.innerHTML = "";
+  channels.forEach((channel, indexChannel) => {
+    channel.messages.forEach((msg, indexMessage) => {
+      if (msg.text.includes(searchTerm)) {
+        let html = "";
+        html +=
+          '<div class="message" onclick="goToMessage(' +
+          indexChannel +
+          ", " +
+          indexMessage +
+          ')">';
+        html +=
+          '<p class="meta">' +
+          msg.username +
+          " <span>" +
+          msg.time +
+          "</span></p>";
+        html += '<p class="text">' + msg.text + "</p>";
+        html += "</div>";
+        searchContainer.insertAdjacentHTML("beforeend", html);
+      }
+    });
+  });
+});
 
 function outputMessages(msg) {
+  //Print del mensaje
 
+  let html = "";
+  html += '<div class="message">';
+  html +=
+    '<p class="meta">' + msg.username + " <span>" + msg.time + "</span></p>";
+  html += '<p class="text">' + msg.text + "</p>";
+  html += "</div>";
 
-    //Div del mensaje
+  //Insert del mensaje en el DOM
 
-    let html = '';
-    html += '<div class="message">';
-    html += '<p class="meta">' + msg.username + ' <span>' + msg.time + '</span></p>';
-    html += '<p class="text">' + msg.text + '</p>';
-    html += '</div>';
+  messageContainer.insertAdjacentHTML("beforeend", html);
 
-    //Insert del mensaje en el DOM
+  document.querySelector(".message:last-child").scrollIntoView({
+    behavior: "smooth",
+  });
+}
 
-    messageContainer.insertAdjacentHTML('beforeend', html);
+//Canal default en el que se guardaran los mensajes
 
-    document.querySelector('.message:last-child').scrollIntoView({
-        behavior: 'smooth'
-    });
-
-};
-
-    //Canal default en el que se guardaran los mensajes
-
-const channels = [{name:'General', messages:[]}];
+const channels = [{ name: "General", messages: [] }];
 let currentChannel = 0;
 
-    //Div de los canales
+//Div de los canales
 
 function printChannel(index) {
-    const channel = channels[index]
-    const channelContainer = document.querySelector('#channels')
-    let html = '';
-    html += '<h2 onclick="changeChannel('+ index +')">' + channel.name + '</h2>'
-    channelContainer.insertAdjacentHTML('beforeend', html)
-};
-    //Selector de canal que guarda los mensajes
+  const channel = channels[index];
+  const channelContainer = document.querySelector("#channels");
+  let html = "";
+  html +=
+    '<h2 onclick="changeChannel(' + index + ')">' + channel.name + "</h2>";
+  channelContainer.insertAdjacentHTML("beforeend", html);
+}
+//Selector de canal que guarda los mensajes
 
 function changeChannel(index) {
-    const previousChannelElement = document.querySelector('#channels h2:nth-child('+(currentChannel+1)+')')
-    previousChannelElement.classList.remove('active')
+  const previousChannelElement = document.querySelector(
+    "#channels h2:nth-child(" + (currentChannel + 1) + ")"
+  );
+  previousChannelElement.classList.remove("active");
 
-    const selectedChannelElement = document.querySelector('#channels h2:nth-child('+(index+1)+')')
-    selectedChannelElement.classList.add('active')
+  const selectedChannelElement = document.querySelector(
+    "#channels h2:nth-child(" + (index + 1) + ")"
+  );
+  selectedChannelElement.classList.add("active");
 
-    currentChannel = index
-    messageContainer.innerHTML= ''
-    channels[currentChannel].messages.forEach((msg)=>{
-    outputMessages(msg)
-})
-};
+  currentChannel = index;
+  messageContainer.innerHTML = "";
+  channels[currentChannel].messages.forEach((msg) => {
+    outputMessages(msg);
+  });
+}
+
+//Creador de canales
 
 function createChannel() {
-const channelName = prompt('Introduce el nombre del canal, amore')
-if (channelName) {
-    channels.push({name:channelName, messages:[]})
-    printChannel(channels.length-1)
+  const channelName = prompt("Introduce el nombre del canal");
+  if (channelName) {
+    channels.push({ name: channelName, messages: [] });
+    printChannel(channels.length - 1);
+  }
 }
-};
 
 printChannel(currentChannel);
 changeChannel(currentChannel);
